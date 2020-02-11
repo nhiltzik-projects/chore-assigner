988ea9c8ae31b255bc356b9dd8307910508c0866
nhiltzik-projects <nhiltzik.github@gmail.com>
2020-02-10 22:23:09 -0500

    updated assignment algorithm

--- assigner.py
+++ assigner.py
@@ -1,36 +1,35 @@
 from datetime import datetime
 from datetime import date
+import numpy as np
 #print(datetime.date(datetime.now()))
 
-REF_DATE = date(2020,2,14)
+REF_DATE = date(2020,2,7)
 
 def weeks_between(today):
     return round(abs((today-REF_DATE).days/7))
 
 def assign_task(week_number):
+    #assigns chores to each roommate based on week
+
+    # chores
     tasks = ["Kitchen", "Garbage", "Living Room", "Hallway"]
 
-    #phone_dictionary = {"Sruli":123, "Alex":123, "Noam":123, "Ariel":123}
+    #list of roommates
+    roommate_array = np.array(["Sruli", "Alex", "Noam", "Ariel"])
 
-    #roommates_permutations = [["Sruli", "Alex", "Noam", "Ariel"],
-    #           ["Alex", "Noam", "Ariel", "Sruli"],
-    #           ["Noam", "Ariel", "Sruli", "Alex"], 
-    #           ["Ariel", "Sruli", "Alex", "Noam"]]
+    #shuffles roomates per week in the cycle
+    shuffled_roommates = np.roll(roommate_array, week_number % 4)
 
-    
-
-    this_week = roommates_permutations[week_number % 4]
-    return dict(zip(this_week, tasks))
-
-
-
-
-    
-
+    return dict(zip(list(shuffled_roommates), tasks))
 
 
 def main():
     today = datetime.date(datetime.now())
-    print(weeks_between(today))
+    
+    week_number = weeks_between(today)
+
+    print(assign_task(week_number))
+
+    
 
 main()
